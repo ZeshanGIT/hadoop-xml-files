fad6248eff112fe0a1a7204b50b5f32b8de30a96
Divyarani R <69153085+Divyarani30@users.noreply.github.com>
2020-11-19 22:13:56 +0530

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,1 +1,6 @@
 export HADOOP_HOME=/usr/local/hadoop
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
